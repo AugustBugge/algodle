7ab24959737359c22b0e6ae086af32c04dd95f87
Oliver Starup <Oliver@starup99.dk>
2025-10-14 10:50:06 +0200

    Removed buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@
 import { AlgorithmGuesser } from "@/components/ui/algorithmGuesser";
 import { Button } from "@/components/ui/button";
 
-
-
 export default function Home() {
   const correctAlgorithm = "Dijkstra's algorithm";
 
@@ -12,8 +10,6 @@ export default function Home() {
       <div className="space-y-4 text-center">
         <h1 className="text-3xl font-bold tracking-tight">algodle</h1>
         <p className="text-muted-foreground">Next.js + Tailwind + shadcn/ui</p>
-        <Button>It works</Button>
-        <Button> It sure does </Button>
         <AlgorithmGuesser />
       </div>
     </main>
